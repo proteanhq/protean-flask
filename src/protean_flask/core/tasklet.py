eeb33f4b90c44aeabcaf23d6fd33b3f047f240a8
Subhash Bhushan <subhash.bhushan@gmail.com>
2018-08-02 10:03:53 +0530

    Use Repository Factory in Tasklet

--- src/protean_flask/core/tasklet.py
+++ src/protean_flask/core/tasklet.py
@@ -3,8 +3,8 @@
 import inflect
 import inflection
 from flask import jsonify
-from protean.transport import Status
-from protean.usecase import Tasklet
+from protean.core.transport import Status
+from protean.core.usecase import Tasklet
 
 INFLECTOR = inflect.engine()
 
@@ -13,7 +13,7 @@ class APITasklet:
     """Utility class to execute UseCases"""
 
     @classmethod
-    def perform(cls, repository_factory, cls_entity, cls_usecase, cls_request_object,
+    def perform(cls, repo_factory, cls_entity, cls_usecase, cls_request_object,
                 cls_serializer, payload, many=False, no_serialization=False):
         """This method bundles all essential artifacts and initiates usecase execution"""
 
@@ -25,9 +25,9 @@ class APITasklet:
             assert no_serialization is True
         else:
             serializer = cls_serializer(many=many)
-            serializer.context = {'repo_factory': repository_factory}
+            serializer.context = {'repo_factory': repo_factory}
 
-        response_object = Tasklet.perform(repository_factory, cls_entity, cls_usecase,
+        response_object = Tasklet.perform(repo_factory, cls_entity, cls_usecase,
                                           cls_request_object, payload, many)
 
         # FIXME Check for Response object value
@@ -35,14 +35,15 @@ class APITasklet:
             if no_serialization:
                 return response_object.value['data']
             else:
+                import pdb; pdb.set_trace()
                 result = serializer.dump(response_object.value['data'])
                 return jsonify({INFLECTOR.plural(resource): result.data,
                                 "total": response_object.value['total'],
                                 "page": response_object.value['page']}), \
-                    Status(response_object.type)
+                    Status(response_object.code).value
         else:
             if no_serialization:
                 return response_object.value
             else:
                 result = serializer.dump(response_object.value)
-                return jsonify({resource: result.data}), Status(response_object.type)
+                return jsonify({resource: result.data}), Status(response_object.code).value
